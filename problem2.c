26bcae519888ee12c3f972d185f3a6d1b55eec11
SujoyDU <debnath.sujoy@hotmail.com>
2021-05-24 14:07:21 -0400

    working with printf vulnerability

--- problem2.c
+++ problem2.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-	const int n = 12;
+int flag = 0;
+
+int main() {
+  const int n = 128;
 	char buffer[n];
 	fgets(buffer,n,stdin);
 	printf(buffer);
+  printf("\nflag Addr: ");
+  printf("%p\n", &flag);
+  if (flag == 1337) {
+        puts("Enjoy your shell!");
+        system("/bin/sh");}
+    else {
+        puts("Not allowed");
+    }
+    return 0;
 	return 0;
 }
+//gcc -fno-stack-protector -z exestack -o victim problem2.c 

